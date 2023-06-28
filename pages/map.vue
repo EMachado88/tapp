3d983c2999acdb40bdf7d82dc1150846685af771
Emanuel Machado <emanuel@emachado.pt>
2023-06-28 21:49:19 +0100

    Base pages

---
+++ pages/map.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Map</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MapPage'
+})
+</script>
---
+++ pages/settings.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Settings</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SettingsPage'
+})
+</script>
