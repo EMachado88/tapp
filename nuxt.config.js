442b2c5a189df55831fc3a30076bdcbbad8833d6
Emanuel Machado <emanuel@emachado.pt>
2023-07-08 14:53:31 +0100

    Fixed router base on prod

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,10 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
+  router: {
+    base: process.env.NODE_ENV === 'production' ? '/tapp/' : '/'
+  },
+
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
