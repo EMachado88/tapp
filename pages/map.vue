4cdbc27de7f02c5e629100c3aa1280f7899e5e3e
Emanuel Machado <emanuel@emachado.pt>
2023-07-08 11:18:09 +0100

    Fix for client, placeholder for center map

--- pages/map.vue
+++ pages/map.vue
@@ -1,29 +1,46 @@
 <template>
-  <GMap
-    ref="map"
-    :center="mapCenter"
-    :zoom="18"
-    :options="{
-      mapTypeControl: false,
-      zoomControl: false,
-      disableDefaultUI: true
-    }"
-  >
-    <GMapMarker
-      :position="userPosition"
-      :clickable="true"
-      :options="{ icon: '/images/rec.png' }"
-      @click="mapCenter = userPosition"
-    />
+  <div id="map-wrap">
+    <ClientOnly fallback-tag="span" fallback="Loading map...">
+      <GMap
+        ref="map"
+        :center="mapCenter"
+        :zoom="18"
+        :options="{
+          mapTypeControl: false,
+          zoomControl: false,
+          disableDefaultUI: true
+        }"
+      >
+        <GMapMarker
+          :position="userPosition"
+          :clickable="true"
+          :options="{ icon: '/images/rec.png' }"
+          @click="mapCenter = userPosition"
+        />
 
-    <GMapMarker
-      v-for="(tap, index) in taps"
-      :key="index"
-      :position="tap.position"
-      :clickable="true"
-      @click="mapCenter = tap.position"
-    />
-  </GMap>
+        <GMapMarker
+          v-for="(tap, index) in taps"
+          :key="index"
+          :position="tap.position"
+          :clickable="true"
+          @click="mapCenter = tap.position"
+        />
+      </GMap>
+    </ClientOnly>
+
+    <v-btn
+      color="red"
+      class="center-button lighten-1 elevation-5"
+      fab
+      dark
+      absolute
+      bottom
+      right
+      @click="center()"
+    >
+      <v-icon>mdi-crosshairs-gps</v-icon>
+    </v-btn>
+  </div>
 </template>
 
 <script lang="ts">
@@ -36,8 +53,8 @@ export default defineComponent({
   data () {
     return {
       taps,
-      mapCenter: { lat: 10, lng: 10 },
-      userPosition: { lat: 10, lng: 10 },
+      mapCenter: { lat: 0, lng: 0 },
+      userPosition: { lat: 0, lng: 0 },
       watchId: 0
     }
   },
@@ -66,19 +83,36 @@ export default defineComponent({
           lng: position.coords.longitude
         }
         this.userPosition = positionLatLng
+      },
+      error => console.error(error), // eslint-disable-line
+      {
+        enableHighAccuracy: true
       })
+    },
+    center () {
+      this.mapCenter = this.userPosition
     }
   }
 })
 </script>
 
 <style lang="scss">
-.GMap {
+#map-wrap {
+  position: relative;
   width: 100%;
   height: 100%;
-}
 
-.GMap__Wrapper {
-  height: 100%;
+  .GMap {
+    width: 100%;
+    height: 100%;
+  }
+
+  .GMap__Wrapper {
+    height: 100%;
+  }
+
+  .center-button {
+    bottom: 20px;
+  }
 }
 </style>
