97728f960087aefbb18047852d70b06654f2e062
Emanuel Machado <emanuel@emachado.pt>
2023-07-08 20:22:54 +0100

    Title test to get ENV

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - Tapp',
-    title: 'Tapp',
+    title: process.env.NODE_ENV,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
