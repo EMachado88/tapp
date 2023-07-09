39ac0813044461adb3259727295f86de55b6a6e1
Emanuel Machado <emanuel@emachado.pt>
2023-07-09 09:09:13 +0100

    Settings page title

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,5 +1,11 @@
 <template>
-  <v-container><h1>Settings</h1></v-container>
+  <v-container>
+    <h4 class="text-h4">
+      <v-icon class="mr-2" size="32px">
+        mdi-cog
+      </v-icon> Settings
+    </h4>
+  </v-container>
 </template>
 
 <script lang="ts">
